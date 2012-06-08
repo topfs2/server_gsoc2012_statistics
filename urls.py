80240fdc91eb279181223095794e69665d6e25c7
Tobias Arrskog <topfs2@xbmc.org>
2012-06-08 12:51:07 +0200

    Added url linking to episode views

--- urls.py
+++ urls.py
@@ -1,17 +1,8 @@
-from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls.defaults import patterns, url
+from djangorestframework.views import ListOrCreateModelView, InstanceModelView
+from views import EpisodeRoot, EpisodeInstance
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'gsoc2012_statistics.views.home', name='home'),
-    # url(r'^gsoc2012_statistics/', include('gsoc2012_statistics.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+	url(r'^episodes$', EpisodeRoot.as_view(), name='episodes-root'),
+	url(r'^episode/([a-fA-F0-9]+)$', EpisodeInstance.as_view(), name='episode-instance'),
 )
