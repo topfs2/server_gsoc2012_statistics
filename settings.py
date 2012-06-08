8282b906202854c345caa94729d42fbeadba25e0
Tobias Arrskog <topfs2@xbmc.org>
2012-06-08 15:28:18 +0200

    Have settings use real repo name as ROOT_URLCONF

--- settings.py
+++ settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'gsoc2012_statistics.urls'
+ROOT_URLCONF = 'server_gsoc2012_statistics.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
